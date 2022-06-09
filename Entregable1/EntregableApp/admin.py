5cc26c854ca82ea32add57c260d856b96ac5300c
TomasLx <langenekertomas@gmail.com>
2022-06-08 22:19:33 -0300

    fase 5 Formulario bienes

--- Entregable1/EntregableApp/admin.py
+++ Entregable1/EntregableApp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Familiares, SegundosFamiliares, BienesFamiliares
 # Register your models here.
+admin.site.register(Familiares)
+admin.site.register(SegundosFamiliares)
+admin.site.register(BienesFamiliares)
--- Entregable1/EntregableApp/models.py
+++ Entregable1/EntregableApp/models.py
@@ -10,6 +10,8 @@ class Familiares(models.Model):
     apellido = models.CharField(max_length=50, default = None)
     fechaDeNacimiento = models.DateField(default = None)
     edad = models.IntegerField(default = None)
+    def __str__(self):
+        return f'{self.nombre}+" "+str{self.apellido}" "+str{self.rol}+str{self.fechaDeNacimiento}" "+str{self.edad}'
 class SegundosFamiliares(models.Model):
     nombre = models.CharField(max_length=50, default = None)
     rol = models.CharField(max_length=50, default = None)
@@ -20,6 +22,7 @@ class BienesFamiliares(models.Model):
     nombre = models.CharField(max_length=50, default = None)
     valuacion = models.IntegerField(default = None)
     antigüedad = models.IntegerField(default = None)
-
+    def __str__(self):
+        return f'{self.nombre}+" "+Valuacion{self.valuacion}+Antugüedad{self.antigüedad}'
 
 
